ba58544fb1af8bbb7909020bc25d765420ce93fb
cnleo <github@cosmicnova.de>
2024-08-13 00:12:29 +0200

    Create template_userscript.user.js

---
+++ template_userscript.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Nice Name 
+// @description Litle Bit of description here.
+// @description:de Ein wenig beschreibungstext auf DE.
+// @namespace   fancy/namespace/here
+// @include     https://yourdomain.tld/owy
+
+// @version     1.0.0
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @run-at      document-end
+// ==/UserScript==
+
+(function(){
+
+	// do things here
+	
+})();
